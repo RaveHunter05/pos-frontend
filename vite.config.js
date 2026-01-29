cadee2dc5748a7cc24dcd8176911562c2148bf08
RaveHunter05 <paulsotelor@yahoo.es>
2026-01-28 20:14:45 -0600

    fix: finally properly installing tailwind

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		server: {
+			port: 3000,
+		},
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
+		define: {
+			__API_BASE_URL__: JSON.stringify(env.VITE_API_BASE_URL),
+		},
+	};
+});
